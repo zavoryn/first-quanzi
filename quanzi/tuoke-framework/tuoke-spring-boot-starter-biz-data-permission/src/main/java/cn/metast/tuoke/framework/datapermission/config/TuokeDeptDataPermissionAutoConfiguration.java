cf90ac64a7fb911a2d00fc56fe7cd81c532d4933
Ming Chen <chenming202503@163.com>
2025-10-10 07:52:02 +0800

    perf: optimize database connection pool

---
+++ quanzi/tuoke-framework/tuoke-spring-boot-starter-biz-data-permission/src/main/java/cn/metast/tuoke/framework/datapermission/config/TuokeDeptDataPermissionAutoConfiguration.java
@@ -0,0 +1,34 @@
+package cn.metast.tuoke.framework.datapermission.config;
+
+import cn.metast.tuoke.framework.datapermission.core.rule.dept.DeptDataPermissionRule;
+import cn.metast.tuoke.framework.datapermission.core.rule.dept.DeptDataPermissionRuleCustomizer;
+import cn.metast.tuoke.framework.security.core.LoginUser;
+import cn.metast.tuoke.module.system.api.permission.PermissionApi;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+/**
+ * 基于部门的数据权限 AutoConfiguration
+ *
+ * @author metast.cn
+ */
+@AutoConfiguration
+@ConditionalOnClass(LoginUser.class)
+@ConditionalOnBean(value = {PermissionApi.class, DeptDataPermissionRuleCustomizer.class})
+public class TuokeDeptDataPermissionAutoConfiguration {
+
+    @Bean
+    public DeptDataPermissionRule deptDataPermissionRule(PermissionApi permissionApi,
+                                                         List<DeptDataPermissionRuleCustomizer> customizers) {
+        // 创建 DeptDataPermissionRule 对象
+        DeptDataPermissionRule rule = new DeptDataPermissionRule(permissionApi);
+        // 补全表配置
+        customizers.forEach(customizer -> customizer.customize(rule));
+        return rule;
+    }
+
+}
