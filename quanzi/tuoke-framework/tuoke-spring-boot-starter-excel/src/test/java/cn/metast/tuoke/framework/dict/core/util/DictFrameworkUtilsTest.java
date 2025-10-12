b3a9fd5a9d35f994dc25425a3484f613c1fe48bb
Ming Chen <chenming202503@163.com>
2025-10-13 07:13:12 +0800

    feat: add admin dashboard API

---
+++ quanzi/tuoke-framework/tuoke-spring-boot-starter-excel/src/test/java/cn/metast/tuoke/framework/dict/core/util/DictFrameworkUtilsTest.java
@@ -0,0 +1,51 @@
+package cn.metast.tuoke.framework.dict.core.util;
+
+import cn.metast.tuoke.framework.common.enums.CommonStatusEnum;
+import cn.metast.tuoke.framework.dict.core.DictFrameworkUtils;
+import cn.metast.tuoke.framework.test.core.ut.BaseMockitoUnitTest;
+import cn.metast.tuoke.module.system.api.dict.DictDataApi;
+import cn.metast.tuoke.module.system.api.dict.dto.DictDataRespDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static cn.metast.tuoke.framework.test.core.util.RandomUtils.randomPojo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+/**
+ * {@link DictFrameworkUtils} 的单元测试
+ */
+public class DictFrameworkUtilsTest extends BaseMockitoUnitTest {
+
+    @Mock
+    private DictDataApi dictDataApi;
+
+    @BeforeEach
+    public void setUp() {
+        DictFrameworkUtils.init(dictDataApi);
+    }
+
+    @Test
+    public void testGetDictDataLabel() {
+        // mock 数据
+        DictDataRespDTO dataRespDTO = randomPojo(DictDataRespDTO.class, o -> o.setStatus(CommonStatusEnum.ENABLE.getStatus()));
+        // mock 方法
+        when(dictDataApi.getDictData(dataRespDTO.getDictType(), dataRespDTO.getValue())).thenReturn(dataRespDTO);
+
+        // 断言返回值
+        assertEquals(dataRespDTO.getLabel(), DictFrameworkUtils.getDictDataLabel(dataRespDTO.getDictType(), dataRespDTO.getValue()));
+    }
+
+    @Test
+    public void testParseDictDataValue() {
+        // mock 数据
+        DictDataRespDTO resp = randomPojo(DictDataRespDTO.class, o -> o.setStatus(CommonStatusEnum.ENABLE.getStatus()));
+        // mock 方法
+        when(dictDataApi.parseDictData(resp.getDictType(), resp.getLabel())).thenReturn(resp);
+
+        // 断言返回值
+        assertEquals(resp.getValue(), DictFrameworkUtils.parseDictDataValue(resp.getDictType(), resp.getLabel()));
+    }
+
+}
---
+++ quanzi/tuoke-framework/tuoke-spring-boot-starter-job/src/main/java/cn/metast/tuoke/framework/quartz/core/handler/JobHandler.java
@@ -0,0 +1,19 @@
+package cn.metast.tuoke.framework.quartz.core.handler;
+
+/**
+ * 任务处理器
+ *
+ * @author metast.cn
+ */
+public interface JobHandler {
+
+    /**
+     * 执行任务
+     *
+     * @param param 参数
+     * @return 结果
+     * @throws Exception 异常
+     */
+    String execute(String param) throws Exception;
+
+}
