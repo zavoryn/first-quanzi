b6b42a9c1f0e2a258a78a99fcce736ba659418b0
Ming Chen <chenming202503@163.com>
2025-10-14 15:52:46 +0800

    perf: optimize database connection pool

---
+++ quanzi/tuoke-framework/tuoke-spring-boot-starter-test/src/main/java/cn/metast/tuoke/framework/test/core/ut/BaseMockitoUnitTest.java
@@ -0,0 +1,13 @@
+package cn.metast.tuoke.framework.test.core.ut;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * 纯 Mockito 的单元测试
+ *
+ * @author metast.cn
+ */
+@ExtendWith(MockitoExtension.class)
+public class BaseMockitoUnitTest {
+}
