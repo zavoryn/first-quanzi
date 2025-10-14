ba15e4571598d51d97619924d7857a11c4712224
Ming Chen <chenming202503@163.com>
2025-10-14 22:36:29 +0800

    feat: implement user authentication module

---
+++ quanzi/tuoke-framework/tuoke-spring-boot-starter-protection/src/main/java/cn/metast/tuoke/framework/ratelimiter/config/TuokeRateLimiterConfiguration.java
@@ -0,0 +1,55 @@
+package cn.metast.tuoke.framework.ratelimiter.config;
+
+import cn.metast.tuoke.framework.ratelimiter.core.aop.RateLimiterAspect;
+import cn.metast.tuoke.framework.ratelimiter.core.keyresolver.RateLimiterKeyResolver;
+import cn.metast.tuoke.framework.ratelimiter.core.keyresolver.impl.*;
+import cn.metast.tuoke.framework.ratelimiter.core.redis.RateLimiterRedisDAO;
+import cn.metast.tuoke.framework.redis.config.TuokeRedisAutoConfiguration;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@AutoConfiguration(after = TuokeRedisAutoConfiguration.class)
+public class TuokeRateLimiterConfiguration {
+
+    @Bean
+    public RateLimiterAspect rateLimiterAspect(List<RateLimiterKeyResolver> keyResolvers, RateLimiterRedisDAO rateLimiterRedisDAO) {
+        return new RateLimiterAspect(keyResolvers, rateLimiterRedisDAO);
+    }
+
+    @Bean
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public RateLimiterRedisDAO rateLimiterRedisDAO(RedissonClient redissonClient) {
+        return new RateLimiterRedisDAO(redissonClient);
+    }
+
+    // ========== 各种 RateLimiterRedisDAO Bean ==========
+
+    @Bean
+    public DefaultRateLimiterKeyResolver defaultRateLimiterKeyResolver() {
+        return new DefaultRateLimiterKeyResolver();
+    }
+
+    @Bean
+    public UserRateLimiterKeyResolver userRateLimiterKeyResolver() {
+        return new UserRateLimiterKeyResolver();
+    }
+
+    @Bean
+    public ClientIpRateLimiterKeyResolver clientIpRateLimiterKeyResolver() {
+        return new ClientIpRateLimiterKeyResolver();
+    }
+
+    @Bean
+    public ServerNodeRateLimiterKeyResolver serverNodeRateLimiterKeyResolver() {
+        return new ServerNodeRateLimiterKeyResolver();
+    }
+
+    @Bean
+    public ExpressionRateLimiterKeyResolver expressionRateLimiterKeyResolver() {
+        return new ExpressionRateLimiterKeyResolver();
+    }
+
+}
