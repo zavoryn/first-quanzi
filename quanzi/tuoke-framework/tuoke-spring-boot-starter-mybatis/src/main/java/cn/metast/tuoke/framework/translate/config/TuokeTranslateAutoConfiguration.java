2b495a162d3d71a37cc759bf7fe03888b636283e
Ming Chen <chenming202503@163.com>
2025-10-14 10:05:09 +0800

    feat: implement OAuth2 third-party login

---
+++ quanzi/tuoke-framework/tuoke-spring-boot-starter-mybatis/src/main/java/cn/metast/tuoke/framework/translate/config/TuokeTranslateAutoConfiguration.java
@@ -0,0 +1,18 @@
+package cn.metast.tuoke.framework.translate.config;
+
+import cn.metast.tuoke.framework.translate.core.TranslateUtils;
+import com.fhs.trans.service.impl.TransService;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+public class TuokeTranslateAutoConfiguration {
+
+    @Bean
+    @SuppressWarnings({"InstantiationOfUtilityClass", "SpringJavaInjectionPointsAutowiringInspection"})
+    public TranslateUtils translateUtils(TransService transService) {
+        TranslateUtils.init(transService);
+        return new TranslateUtils();
+    }
+
+}
