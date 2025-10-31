b6b94d097b777665d0f76970bb229e19007ae91d
Ming Chen <chenming202503@163.com>
2025-10-31 22:39:19 +0800

    perf: optimize database connection pool

---
+++ quanzi/tuoke-module-bpm/tuoke-module-bpm-biz/src/main/java/cn/metast/tuoke/module/bpm/controller/admin/definition/vo/form/BpmFormPageReqVO.java
@@ -0,0 +1,16 @@
+package cn.metast.tuoke.module.bpm.controller.admin.definition.vo.form;
+
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Schema(description = "管理后台 - 动态表单分页 Request VO")
+@Data
+public class BpmFormPageReqVO extends PageParam {
+
+    @Schema(description = "表单名称", example = "元圈")
+    private String name;
+
+}
