02a4b3f42a931c135074d65c44daca4567e185f8
Ming Chen <chenming202503@163.com>
2025-11-03 10:37:37 +0800

    feat: implement OAuth2 third-party login

---
+++ quanzi/tuoke-module-bpm/tuoke-module-bpm-biz/src/main/java/cn/metast/tuoke/module/bpm/dal/redis/RedisKeyConstants.java
@@ -0,0 +1,15 @@
+package cn.metast.tuoke.module.bpm.dal.redis;
+
+/**
+ * BPM Redis Key 枚举类
+ *
+ * @author metast.cn
+ */
+public interface RedisKeyConstants {
+
+    /**
+     * 流程 ID 的缓存
+     */
+    String BPM_PROCESS_ID = "bpm:process_id:";
+
+}
