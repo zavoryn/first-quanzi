0c713020912ba3a5a2c94a40cfe48313aba35d49
Ming Chen <chenming202503@163.com>
2025-11-03 19:59:03 +0800

    feat: add admin dashboard API

---
+++ quanzi/tuoke-module-bpm/tuoke-module-bpm-biz/src/main/java/cn/metast/tuoke/module/bpm/controller/admin/task/vo/task/BpmTaskPageReqVO.java
@@ -0,0 +1,28 @@
+package cn.metast.tuoke.module.bpm.controller.admin.task.vo.task;
+
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import cn.metast.tuoke.framework.common.util.date.DateUtils;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Schema(description = "管理后台 - 流程任务的的分页 Request VO") // 待办、已办，都使用该分页
+@Data
+public class BpmTaskPageReqVO extends PageParam {
+
+    @Schema(description = "流程任务名", example = "元圈")
+    private String name;
+
+    @Schema(description = "流程分类", example = "1")
+    private String category;
+
+    @Schema(description = "流程定义的标识", example = "2048")
+    private String processDefinitionKey; // 精准匹配
+
+    @Schema(description = "创建时间")
+    @DateTimeFormat(pattern = DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private LocalDateTime[] createTime;
+
+}
