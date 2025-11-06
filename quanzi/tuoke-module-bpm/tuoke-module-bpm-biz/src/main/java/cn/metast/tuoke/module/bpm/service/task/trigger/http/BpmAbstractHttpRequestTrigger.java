6868d84b82a6b7e5d6618ca0b623f247251226df
Ming Chen <chenming202503@163.com>
2025-11-06 20:32:16 +0800

    feat: add like favorite and bookmark features

---
+++ quanzi/tuoke-module-bpm/tuoke-module-bpm-biz/src/main/java/cn/metast/tuoke/module/bpm/service/task/trigger/http/BpmAbstractHttpRequestTrigger.java
@@ -0,0 +1,14 @@
+package cn.metast.tuoke.module.bpm.service.task.trigger.http;
+
+import cn.metast.tuoke.module.bpm.service.task.trigger.BpmTrigger;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * BPM 发送 HTTP 请求触发器抽象类
+ *
+ * @author jason
+ */
+@Slf4j
+public abstract class BpmAbstractHttpRequestTrigger implements BpmTrigger {
+
+}
