276601379fe01fa628a5501ab193104ecf01cf4a
Ming Chen <chenming202503@163.com>
2025-11-24 18:03:06 +0800

    refactor: optimize feed query with cursor pagination

---
+++ quanzi/tuoke-module-crm/tuoke-module-crm-biz/src/main/java/cn/metast/tuoke/module/crm/dal/mysql/business/CrmBusinessStatusTypeMapper.java
@@ -0,0 +1,27 @@
+package cn.metast.tuoke.module.crm.dal.mysql.business;
+
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.crm.dal.dataobject.business.CrmBusinessStatusTypeDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商机状态组 Mapper
+ *
+ * @author ljlleo
+ */
+@Mapper
+public interface CrmBusinessStatusTypeMapper extends BaseMapperX<CrmBusinessStatusTypeDO> {
+
+    default PageResult<CrmBusinessStatusTypeDO> selectPage(PageParam reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<CrmBusinessStatusTypeDO>()
+                .orderByDesc(CrmBusinessStatusTypeDO::getId));
+    }
+
+    default CrmBusinessStatusTypeDO selectByName(String name) {
+        return selectOne(CrmBusinessStatusTypeDO::getName, name);
+    }
+
+}
