cecc7a29d55f289bf9e472c327d56eeed64b579f
Ming Chen <chenming202503@163.com>
2025-11-26 17:19:48 +0800

    test: add performance benchmark tests

---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-api/src/main/java/cn/metast/tuoke/module/erp/enums/DictTypeConstants.java
@@ -0,0 +1,13 @@
+package cn.metast.tuoke.module.erp.enums;
+
+/**
+ * ERP 字典类型的枚举类
+ *
+ * @author metast.cn
+ */
+public interface DictTypeConstants {
+
+    String AUDIT_STATUS = "erp_audit_status"; // 审核状态
+    String STOCK_RECORD_BIZ_TYPE = "erp_stock_record_biz_type"; // 库存明细的业务类型
+
+}
