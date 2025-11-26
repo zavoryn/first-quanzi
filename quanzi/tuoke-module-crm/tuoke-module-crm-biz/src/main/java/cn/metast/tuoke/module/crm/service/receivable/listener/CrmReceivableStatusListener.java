8b0c169767bdd12b1d1a6875436ba0026f4e9ba4
Ming Chen <chenming202503@163.com>
2025-11-26 15:35:22 +0800

    feat: add post publishing and draft API

---
+++ quanzi/tuoke-module-crm/tuoke-module-crm-biz/src/main/java/cn/metast/tuoke/module/crm/service/receivable/listener/CrmReceivableStatusListener.java
@@ -0,0 +1,31 @@
+package cn.metast.tuoke.module.crm.service.receivable.listener;
+
+import cn.metast.tuoke.module.bpm.event.BpmProcessInstanceStatusEvent;
+import cn.metast.tuoke.module.bpm.event.BpmProcessInstanceStatusEventListener;
+import cn.metast.tuoke.module.crm.service.receivable.CrmReceivableService;
+import cn.metast.tuoke.module.crm.service.receivable.CrmReceivableServiceImpl;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+
+/**
+ * 回款审批的结果的监听器实现类
+ *
+ * @author HUIHUI
+ */
+@Component
+public class CrmReceivableStatusListener extends BpmProcessInstanceStatusEventListener {
+
+    @Resource
+    private CrmReceivableService receivableService;
+
+    @Override
+    public String getProcessDefinitionKey() {
+        return CrmReceivableServiceImpl.BPM_PROCESS_DEFINITION_KEY;
+    }
+
+    @Override
+    public void onEvent(BpmProcessInstanceStatusEvent event) {
+        receivableService.updateReceivableAuditStatus(Long.parseLong(event.getBusinessKey()), event.getStatus());
+    }
+
+}
