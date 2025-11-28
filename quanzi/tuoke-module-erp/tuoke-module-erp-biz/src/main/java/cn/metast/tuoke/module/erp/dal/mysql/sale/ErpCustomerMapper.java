b958c9c1368dfa83ff4775a03711222d945608c0
Ming Chen <chenming202503@163.com>
2025-11-28 21:24:04 +0800

    feat: add like favorite and bookmark features

---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-biz/src/main/java/cn/metast/tuoke/module/erp/dal/mysql/sale/ErpCustomerMapper.java
@@ -0,0 +1,32 @@
+package cn.metast.tuoke.module.erp.dal.mysql.sale;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.erp.controller.admin.sale.vo.customer.ErpCustomerPageReqVO;
+import cn.metast.tuoke.module.erp.dal.dataobject.sale.ErpCustomerDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * ERP 客户 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface ErpCustomerMapper extends BaseMapperX<ErpCustomerDO> {
+
+    default PageResult<ErpCustomerDO> selectPage(ErpCustomerPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<ErpCustomerDO>()
+                .likeIfPresent(ErpCustomerDO::getName, reqVO.getName())
+                .eqIfPresent(ErpCustomerDO::getMobile, reqVO.getMobile())
+                .eqIfPresent(ErpCustomerDO::getTelephone, reqVO.getTelephone())
+                .orderByDesc(ErpCustomerDO::getId));
+    }
+
+    default List<ErpCustomerDO> selectListByStatus(Integer status) {
+        return selectList(ErpCustomerDO::getStatus, status);
+    }
+
+}
