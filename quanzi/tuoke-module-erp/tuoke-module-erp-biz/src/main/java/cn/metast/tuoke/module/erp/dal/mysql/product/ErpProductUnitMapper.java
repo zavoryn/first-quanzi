5560cf0c110b14854d530f634937d39e0e3e7ad7
Ming Chen <chenming202503@163.com>
2025-11-28 14:13:22 +0800

    test: add service layer integration tests

---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-biz/src/main/java/cn/metast/tuoke/module/erp/dal/mysql/product/ErpProductUnitMapper.java
@@ -0,0 +1,35 @@
+package cn.metast.tuoke.module.erp.dal.mysql.product;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.module.erp.controller.admin.product.vo.unit.ErpProductUnitPageReqVO;
+import cn.metast.tuoke.module.erp.dal.dataobject.product.ErpProductUnitDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * ERP 产品单位 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface ErpProductUnitMapper extends BaseMapperX<ErpProductUnitDO> {
+
+    default PageResult<ErpProductUnitDO> selectPage(ErpProductUnitPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<ErpProductUnitDO>()
+                .likeIfPresent(ErpProductUnitDO::getName, reqVO.getName())
+                .eqIfPresent(ErpProductUnitDO::getStatus, reqVO.getStatus())
+                .orderByDesc(ErpProductUnitDO::getId));
+    }
+
+    default ErpProductUnitDO selectByName(String name) {
+        return selectOne(ErpProductUnitDO::getName, name);
+    }
+
+    default List<ErpProductUnitDO> selectListByStatus(Integer status) {
+        return selectList(ErpProductUnitDO::getStatus, status);
+    }
+
+}
---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-biz/src/main/java/cn/metast/tuoke/module/erp/dal/mysql/purchase/ErpSupplierMapper.java
@@ -0,0 +1,32 @@
+package cn.metast.tuoke.module.erp.dal.mysql.purchase;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.module.erp.controller.admin.purchase.vo.supplier.ErpSupplierPageReqVO;
+import cn.metast.tuoke.module.erp.dal.dataobject.purchase.ErpSupplierDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * ERP 供应商 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface ErpSupplierMapper extends BaseMapperX<ErpSupplierDO> {
+
+    default PageResult<ErpSupplierDO> selectPage(ErpSupplierPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<ErpSupplierDO>()
+                .likeIfPresent(ErpSupplierDO::getName, reqVO.getName())
+                .likeIfPresent(ErpSupplierDO::getMobile, reqVO.getMobile())
+                .likeIfPresent(ErpSupplierDO::getTelephone, reqVO.getTelephone())
+                .orderByDesc(ErpSupplierDO::getId));
+    }
+
+    default List<ErpSupplierDO> selectListByStatus(Integer status) {
+        return selectList(ErpSupplierDO::getStatus, status);
+    }
+
+}
