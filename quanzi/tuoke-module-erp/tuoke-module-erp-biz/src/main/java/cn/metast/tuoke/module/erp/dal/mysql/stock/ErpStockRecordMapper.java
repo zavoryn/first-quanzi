53ee46e5784c89b106001c1e078794947ed00773
Ming Chen <chenming202503@163.com>
2025-12-01 19:10:08 +0800

    feat: implement user authentication module

---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-biz/src/main/java/cn/metast/tuoke/module/erp/dal/mysql/stock/ErpStockRecordMapper.java
@@ -0,0 +1,28 @@
+package cn.metast.tuoke.module.erp.dal.mysql.stock;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.erp.controller.admin.stock.vo.record.ErpStockRecordPageReqVO;
+import cn.metast.tuoke.module.erp.dal.dataobject.stock.ErpStockRecordDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ERP 产品库存明细 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface ErpStockRecordMapper extends BaseMapperX<ErpStockRecordDO> {
+
+    default PageResult<ErpStockRecordDO> selectPage(ErpStockRecordPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<ErpStockRecordDO>()
+                .eqIfPresent(ErpStockRecordDO::getProductId, reqVO.getProductId())
+                .eqIfPresent(ErpStockRecordDO::getWarehouseId, reqVO.getWarehouseId())
+                .eqIfPresent(ErpStockRecordDO::getBizType, reqVO.getBizType())
+                .likeIfPresent(ErpStockRecordDO::getBizNo, reqVO.getBizNo())
+                .betweenIfPresent(ErpStockRecordDO::getCreateTime, reqVO.getCreateTime())
+                .orderByDesc(ErpStockRecordDO::getId));
+    }
+
+}
---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-biz/src/main/java/cn/metast/tuoke/module/erp/dal/mysql/stock/ErpWarehouseMapper.java
@@ -0,0 +1,35 @@
+package cn.metast.tuoke.module.erp.dal.mysql.stock;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.module.erp.controller.admin.stock.vo.warehouse.ErpWarehousePageReqVO;
+import cn.metast.tuoke.module.erp.dal.dataobject.stock.ErpWarehouseDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * ERP 仓库 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface ErpWarehouseMapper extends BaseMapperX<ErpWarehouseDO> {
+
+    default PageResult<ErpWarehouseDO> selectPage(ErpWarehousePageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<ErpWarehouseDO>()
+                .likeIfPresent(ErpWarehouseDO::getName, reqVO.getName())
+                .eqIfPresent(ErpWarehouseDO::getStatus, reqVO.getStatus())
+                .orderByDesc(ErpWarehouseDO::getId));
+    }
+
+    default ErpWarehouseDO selectByDefaultStatus() {
+        return selectOne(ErpWarehouseDO::getDefaultStatus, true);
+    }
+
+    default List<ErpWarehouseDO> selectListByStatus(Integer status) {
+        return selectList(ErpWarehouseDO::getStatus, status);
+    }
+
+}
