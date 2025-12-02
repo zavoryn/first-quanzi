7001115d4a6674ca0bdde2ff17fdf51bd590d305
Ming Chen <chenming202503@163.com>
2025-12-02 19:39:33 +0800

    feat: implement OAuth2 third-party login

---
+++ quanzi/tuoke-module-erp/tuoke-module-erp-biz/src/main/java/cn/metast/tuoke/module/erp/dal/redis/RedisKeyConstants.java
@@ -0,0 +1,18 @@
+package cn.metast.tuoke.module.erp.dal.redis;
+
+/**
+ * ERP Redis Key 枚举类
+ *
+ * @author metast.cn
+ */
+public interface RedisKeyConstants {
+
+    /**
+     * 序号的缓存
+     *
+     * KEY 格式：trade_no:{prefix}
+     * VALUE 数据格式：编号自增
+     */
+    String NO = "erp:seq_no:";
+
+}
