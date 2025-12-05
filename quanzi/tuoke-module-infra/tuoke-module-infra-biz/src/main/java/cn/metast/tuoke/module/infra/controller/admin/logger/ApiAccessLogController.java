bb9e24c307f95a8d4188f93606150287949cb1cf
Ming Chen <chenming202503@163.com>
2025-12-05 14:29:29 +0800

    test: add performance benchmark tests

---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/controller/admin/logger/ApiAccessLogController.java
@@ -0,0 +1,60 @@
+package cn.metast.tuoke.module.infra.controller.admin.logger;
+
+import cn.metast.tuoke.framework.apilog.core.annotation.ApiAccessLog;
+import cn.metast.tuoke.framework.common.pojo.CommonResult;
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.common.util.object.BeanUtils;
+import cn.metast.tuoke.framework.excel.core.util.ExcelUtils;
+import cn.metast.tuoke.module.infra.controller.admin.logger.vo.apiaccesslog.ApiAccessLogPageReqVO;
+import cn.metast.tuoke.module.infra.controller.admin.logger.vo.apiaccesslog.ApiAccessLogRespVO;
+import cn.metast.tuoke.module.infra.dal.dataobject.logger.ApiAccessLogDO;
+import cn.metast.tuoke.module.infra.service.logger.ApiAccessLogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+
+import static cn.metast.tuoke.framework.apilog.core.enums.OperateTypeEnum.EXPORT;
+import static cn.metast.tuoke.framework.common.pojo.CommonResult.success;
+
+@Tag(name = "管理后台 - API 访问日志")
+@RestController
+@RequestMapping("/infra/api-access-log")
+@Validated
+public class ApiAccessLogController {
+
+    @Resource
+    private ApiAccessLogService apiAccessLogService;
+
+    @GetMapping("/page")
+    @Operation(summary = "获得API 访问日志分页")
+    @PreAuthorize("@ss.hasPermission('infra:api-access-log:query')")
+    public CommonResult<PageResult<ApiAccessLogRespVO>> getApiAccessLogPage(@Valid ApiAccessLogPageReqVO pageReqVO) {
+        PageResult<ApiAccessLogDO> pageResult = apiAccessLogService.getApiAccessLogPage(pageReqVO);
+        return success(BeanUtils.toBean(pageResult, ApiAccessLogRespVO.class));
+    }
+
+    @GetMapping("/export-excel")
+    @Operation(summary = "导出API 访问日志 Excel")
+    @PreAuthorize("@ss.hasPermission('infra:api-access-log:export')")
+    @ApiAccessLog(operateType = EXPORT)
+    public void exportApiAccessLogExcel(@Valid ApiAccessLogPageReqVO exportReqVO,
+                                        HttpServletResponse response) throws IOException {
+        exportReqVO.setPageSize(PageParam.PAGE_SIZE_NONE);
+        List<ApiAccessLogDO> list = apiAccessLogService.getApiAccessLogPage(exportReqVO).getList();
+        // 导出 Excel
+        ExcelUtils.write(response, "API 访问日志.xls", "数据", ApiAccessLogRespVO.class,
+                BeanUtils.toBean(list, ApiAccessLogRespVO.class));
+    }
+
+}
