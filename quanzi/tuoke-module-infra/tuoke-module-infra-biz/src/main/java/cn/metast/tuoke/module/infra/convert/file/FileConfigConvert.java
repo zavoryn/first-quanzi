0197c5111e9f505970d40674b4d6e2243de188d5
Ming Chen <chenming202503@163.com>
2025-12-06 19:53:54 +0800

    perf: add Redis cache for hot posts

---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/convert/file/FileConfigConvert.java
@@ -0,0 +1,22 @@
+package cn.metast.tuoke.module.infra.convert.file;
+
+import cn.metast.tuoke.module.infra.controller.admin.file.vo.config.FileConfigSaveReqVO;
+import cn.metast.tuoke.module.infra.dal.dataobject.file.FileConfigDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * 文件配置 Convert
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface FileConfigConvert {
+
+    FileConfigConvert INSTANCE = Mappers.getMapper(FileConfigConvert.class);
+
+    @Mapping(target = "config", ignore = true)
+    FileConfigDO convert(FileConfigSaveReqVO bean);
+
+}
