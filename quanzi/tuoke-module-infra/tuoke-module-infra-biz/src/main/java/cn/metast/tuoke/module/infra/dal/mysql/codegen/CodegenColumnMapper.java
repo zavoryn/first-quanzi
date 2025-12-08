21c6e864d61415ed34bbc5eef838ed033af352cd
Ming Chen <chenming202503@163.com>
2025-12-08 12:02:16 +0800

    style: unify API response format

---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/dal/mysql/codegen/CodegenColumnMapper.java
@@ -0,0 +1,24 @@
+package cn.metast.tuoke.module.infra.dal.mysql.codegen;
+
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.infra.dal.dataobject.codegen.CodegenColumnDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CodegenColumnMapper extends BaseMapperX<CodegenColumnDO> {
+
+    default List<CodegenColumnDO> selectListByTableId(Long tableId) {
+        return selectList(new LambdaQueryWrapperX<CodegenColumnDO>()
+                .eq(CodegenColumnDO::getTableId, tableId)
+                .orderByAsc(CodegenColumnDO::getOrdinalPosition));
+    }
+
+    default void deleteListByTableId(Long tableId) {
+        delete(new LambdaQueryWrapperX<CodegenColumnDO>()
+                .eq(CodegenColumnDO::getTableId, tableId));
+    }
+
+}
---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/dal/mysql/config/ConfigMapper.java
@@ -0,0 +1,25 @@
+package cn.metast.tuoke.module.infra.dal.mysql.config;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.infra.controller.admin.config.vo.ConfigPageReqVO;
+import cn.metast.tuoke.module.infra.dal.dataobject.config.ConfigDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ConfigMapper extends BaseMapperX<ConfigDO> {
+
+    default ConfigDO selectByKey(String key) {
+        return selectOne(ConfigDO::getConfigKey, key);
+    }
+
+    default PageResult<ConfigDO> selectPage(ConfigPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<ConfigDO>()
+                .likeIfPresent(ConfigDO::getName, reqVO.getName())
+                .likeIfPresent(ConfigDO::getConfigKey, reqVO.getKey())
+                .eqIfPresent(ConfigDO::getType, reqVO.getType())
+                .betweenIfPresent(ConfigDO::getCreateTime, reqVO.getCreateTime()));
+    }
+
+}
