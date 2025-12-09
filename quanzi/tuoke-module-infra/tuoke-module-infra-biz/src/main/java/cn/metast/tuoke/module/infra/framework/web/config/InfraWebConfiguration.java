64189cf33858cbcf0b39bac3da701eec6316fb5a
Ming Chen <chenming202503@163.com>
2025-12-09 10:57:04 +0800

    refactor: extract base service and DAO layers

---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/framework/web/config/InfraWebConfiguration.java
@@ -0,0 +1,24 @@
+package cn.metast.tuoke.module.infra.framework.web.config;
+
+import cn.metast.tuoke.framework.swagger.config.TuokeSwaggerAutoConfiguration;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * infra 模块的 web 组件的 Configuration
+ *
+ * @author metast.cn
+ */
+@Configuration(proxyBeanMethods = false)
+public class InfraWebConfiguration {
+
+    /**
+     * infra 模块的 API 分组
+     */
+    @Bean
+    public GroupedOpenApi infraGroupedOpenApi() {
+        return TuokeSwaggerAutoConfiguration.buildGroupedOpenApi("infra");
+    }
+
+}
