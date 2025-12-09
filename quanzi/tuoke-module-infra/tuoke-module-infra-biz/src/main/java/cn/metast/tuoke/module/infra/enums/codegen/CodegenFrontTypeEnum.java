41f1ecde736e4b52c0fcaf8fd451af907ac892a4
Ming Chen <chenming202503@163.com>
2025-12-09 10:12:59 +0800

    test: add performance benchmark tests

---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/enums/codegen/CodegenFrontTypeEnum.java
@@ -0,0 +1,26 @@
+package cn.metast.tuoke.module.infra.enums.codegen;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 代码生成的前端类型枚举
+ *
+ * @author metast.cn
+ */
+@AllArgsConstructor
+@Getter
+public enum CodegenFrontTypeEnum {
+
+    VUE2_ELEMENT_UI(10), // Vue2 Element UI 标准模版
+    VUE3_ELEMENT_PLUS(20), // Vue3 Element Plus 标准模版
+    VUE3_VBEN2_ANTD_SCHEMA(30), // Vue3 VBEN2 + ANTD + Schema 模版
+    VUE3_VBEN5_ANTD_SCHEMA(40), // Vue3 VBEN5 + ANTD + schema 模版
+    ;
+
+    /**
+     * 类型
+     */
+    private final Integer type;
+
+}
