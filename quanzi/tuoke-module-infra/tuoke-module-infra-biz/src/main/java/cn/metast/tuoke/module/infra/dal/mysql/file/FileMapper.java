6123b77584ba1807c8fdc78d34943a82a62497e6
Ming Chen <chenming202503@163.com>
2025-12-09 11:25:57 +0800

    perf: optimize database connection pool

---
+++ quanzi/tuoke-module-infra/tuoke-module-infra-biz/src/main/java/cn/metast/tuoke/module/infra/dal/mysql/file/FileMapper.java
@@ -0,0 +1,26 @@
+package cn.metast.tuoke.module.infra.dal.mysql.file;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.infra.controller.admin.file.vo.file.FilePageReqVO;
+import cn.metast.tuoke.module.infra.dal.dataobject.file.FileDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 文件操作 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface FileMapper extends BaseMapperX<FileDO> {
+
+    default PageResult<FileDO> selectPage(FilePageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<FileDO>()
+                .likeIfPresent(FileDO::getPath, reqVO.getPath())
+                .likeIfPresent(FileDO::getType, reqVO.getType())
+                .betweenIfPresent(FileDO::getCreateTime, reqVO.getCreateTime())
+                .orderByDesc(FileDO::getId));
+    }
+
+}
