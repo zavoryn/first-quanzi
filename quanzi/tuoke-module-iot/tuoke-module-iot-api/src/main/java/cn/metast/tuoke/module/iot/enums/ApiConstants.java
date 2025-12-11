1ee594e35546544b5a01e2a40930ca9942d65f21
Ming Chen <chenming202503@163.com>
2025-12-11 09:46:56 +0800

    feat: add like favorite and bookmark features

---
+++ quanzi/tuoke-module-iot/tuoke-module-iot-api/src/main/java/cn/metast/tuoke/module/iot/enums/ApiConstants.java
@@ -0,0 +1,16 @@
+package cn.metast.tuoke.module.iot.enums;
+
+import cn.metast.tuoke.framework.common.enums.RpcConstants;
+
+/**
+ * API 相关的枚举
+ *
+ * @author metast.cn
+ */
+public class ApiConstants {
+
+    public static final String PREFIX = RpcConstants.RPC_API_PREFIX + "/iot";
+
+    public static final String VERSION = "1.0.0";
+
+}
