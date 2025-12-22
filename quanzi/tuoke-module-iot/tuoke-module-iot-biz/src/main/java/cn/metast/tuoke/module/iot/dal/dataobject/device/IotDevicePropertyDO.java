d3c5de66dbe1fd50358be61a12ca2ebd59c6a9bf
Ming Chen <chenming202503@163.com>
2025-12-22 11:21:20 +0800

    test: add performance benchmark tests

---
+++ quanzi/tuoke-module-iot/tuoke-module-iot-biz/src/main/java/cn/metast/tuoke/module/iot/dal/dataobject/device/IotDevicePropertyDO.java
@@ -0,0 +1,35 @@
+package cn.metast.tuoke.module.iot.dal.dataobject.device;
+
+import cn.metast.tuoke.module.iot.dal.redis.device.DevicePropertyRedisDAO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * IoT 设备属性项 Redis DO
+ *
+ * @see cn.metast.tuoke.module.iot.dal.redis.RedisKeyConstants#DEVICE_PROPERTY
+ * @see DevicePropertyRedisDAO
+ *
+ * @author haohao
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class IotDevicePropertyDO {
+
+    /**
+     * 属性值（最新）
+     */
+    private Object value;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+
+}
