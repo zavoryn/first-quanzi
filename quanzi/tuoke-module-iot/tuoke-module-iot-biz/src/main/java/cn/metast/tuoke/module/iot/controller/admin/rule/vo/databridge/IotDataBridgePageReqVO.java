e29cdb7b3a0ba09ed16ff2f969622127cc0faaa6
Ming Chen <chenming202503@163.com>
2025-12-22 15:05:30 +0800

    refactor: extract base service and DAO layers

---
+++ quanzi/tuoke-module-iot/tuoke-module-iot-biz/src/main/java/cn/metast/tuoke/module/iot/controller/admin/rule/vo/databridge/IotDataBridgePageReqVO.java
@@ -0,0 +1,29 @@
+package cn.metast.tuoke.module.iot.controller.admin.rule.vo.databridge;
+
+import cn.metast.tuoke.framework.common.enums.CommonStatusEnum;
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import cn.metast.tuoke.framework.common.validation.InEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+import static cn.metast.tuoke.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
+
+@Schema(description = "管理后台 - IoT 数据桥梁分页 Request VO")
+@Data
+public class IotDataBridgePageReqVO extends PageParam {
+
+    @Schema(description = "桥梁名称", example = "赵六")
+    private String name;
+
+    @Schema(description = "桥梁状态", example = "1")
+    @InEnum(CommonStatusEnum.class)
+    private Integer status;
+
+    @Schema(description = "创建时间")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private LocalDateTime[] createTime;
+
+}
