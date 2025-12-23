c6fc33b035e8f54c360d2209ae7bd08f624c5ff8
Ming Chen <chenming202503@163.com>
2025-12-23 15:48:53 +0800

    feat: add common framework and base utilities

---
+++ quanzi/tuoke-module-iot/tuoke-module-iot-biz/src/main/java/cn/metast/tuoke/module/iot/dal/mysql/device/IotDeviceGroupMapper.java
@@ -0,0 +1,35 @@
+package cn.metast.tuoke.module.iot.dal.mysql.device;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.iot.controller.admin.device.vo.group.IotDeviceGroupPageReqVO;
+import cn.metast.tuoke.module.iot.dal.dataobject.device.IotDeviceGroupDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * IoT 设备分组 Mapper
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface IotDeviceGroupMapper extends BaseMapperX<IotDeviceGroupDO> {
+
+    default PageResult<IotDeviceGroupDO> selectPage(IotDeviceGroupPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<IotDeviceGroupDO>()
+                .likeIfPresent(IotDeviceGroupDO::getName, reqVO.getName())
+                .betweenIfPresent(IotDeviceGroupDO::getCreateTime, reqVO.getCreateTime())
+                .orderByDesc(IotDeviceGroupDO::getId));
+    }
+
+    default List<IotDeviceGroupDO> selectListByStatus(Integer status) {
+        return selectList(IotDeviceGroupDO::getStatus, status);
+    }
+
+    default IotDeviceGroupDO selectByName(String name) {
+        return selectOne(IotDeviceGroupDO::getName, name);
+    }
+
+}
