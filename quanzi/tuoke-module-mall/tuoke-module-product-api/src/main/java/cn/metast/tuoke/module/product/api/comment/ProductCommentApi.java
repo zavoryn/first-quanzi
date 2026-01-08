9d8a4a5ddf03ec037409061097fe1f19b759fd6a
Ming Chen <chenming202503@163.com>
2026-01-08 13:38:09 +0800

    feat: add like favorite and bookmark features

---
+++ quanzi/tuoke-module-mall/tuoke-module-product-api/src/main/java/cn/metast/tuoke/module/product/api/comment/ProductCommentApi.java
@@ -0,0 +1,20 @@
+package cn.metast.tuoke.module.product.api.comment;
+
+import cn.metast.tuoke.module.product.api.comment.dto.ProductCommentCreateReqDTO;
+
+/**
+ * 产品评论 API 接口
+ *
+ * @author HUIHUI
+ */
+public interface ProductCommentApi {
+
+    /**
+     * 创建评论
+     *
+     * @param createReqDTO 评论参数
+     * @return 返回评论创建后的 id
+     */
+    Long createComment(ProductCommentCreateReqDTO createReqDTO);
+
+}
