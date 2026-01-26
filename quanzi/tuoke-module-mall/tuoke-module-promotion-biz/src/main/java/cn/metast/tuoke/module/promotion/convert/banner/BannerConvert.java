27c72e2b51432e8b21ecaa9b39b79972ef739c5f
Ming Chen <chenming202503@163.com>
2026-01-26 09:12:19 +0800

    style: unify API response format

---
+++ quanzi/tuoke-module-mall/tuoke-module-promotion-biz/src/main/java/cn/metast/tuoke/module/promotion/convert/banner/BannerConvert.java
@@ -0,0 +1,31 @@
+package cn.metast.tuoke.module.promotion.convert.banner;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.module.promotion.controller.admin.banner.vo.BannerCreateReqVO;
+import cn.metast.tuoke.module.promotion.controller.admin.banner.vo.BannerRespVO;
+import cn.metast.tuoke.module.promotion.controller.admin.banner.vo.BannerUpdateReqVO;
+import cn.metast.tuoke.module.promotion.controller.app.banner.vo.AppBannerRespVO;
+import cn.metast.tuoke.module.promotion.dal.dataobject.banner.BannerDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface BannerConvert {
+
+    BannerConvert INSTANCE = Mappers.getMapper(BannerConvert.class);
+
+    List<BannerRespVO> convertList(List<BannerDO> list);
+
+    PageResult<BannerRespVO> convertPage(PageResult<BannerDO> pageResult);
+
+    BannerRespVO convert(BannerDO banner);
+
+    BannerDO convert(BannerCreateReqVO createReqVO);
+
+    BannerDO convert(BannerUpdateReqVO updateReqVO);
+
+    List<AppBannerRespVO> convertList01(List<BannerDO> bannerList);
+
+}
---
+++ quanzi/tuoke-module-mall/tuoke-module-promotion-biz/src/main/java/cn/metast/tuoke/module/promotion/convert/bargain/BargainHelpConvert.java
@@ -0,0 +1,46 @@
+package cn.metast.tuoke.module.promotion.convert.bargain;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.common.util.collection.MapUtils;
+import cn.metast.tuoke.module.member.api.user.dto.MemberUserRespDTO;
+import cn.metast.tuoke.module.promotion.controller.admin.bargain.vo.help.BargainHelpRespVO;
+import cn.metast.tuoke.module.promotion.controller.app.bargain.vo.help.AppBargainHelpRespVO;
+import cn.metast.tuoke.module.promotion.dal.dataobject.bargain.BargainHelpDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 砍价助力 Convert
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface BargainHelpConvert {
+
+    BargainHelpConvert INSTANCE = Mappers.getMapper(BargainHelpConvert.class);
+
+    default PageResult<BargainHelpRespVO> convertPage(PageResult<BargainHelpDO> page,
+                                                      Map<Long, MemberUserRespDTO> userMap) {
+        PageResult<BargainHelpRespVO> pageResult = convertPage(page);
+        // 拼接数据
+        pageResult.getList().forEach(record ->
+                MapUtils.findAndThen(userMap, record.getUserId(),
+                        user -> record.setNickname(user.getNickname()).setAvatar(user.getAvatar())));
+        return pageResult;
+    }
+    PageResult<BargainHelpRespVO> convertPage(PageResult<BargainHelpDO> page);
+
+    default List<AppBargainHelpRespVO> convertList(List<BargainHelpDO> helps,
+                                                   Map<Long, MemberUserRespDTO> userMap) {
+        List<AppBargainHelpRespVO> helpVOs = convertList02(helps);
+        helpVOs.forEach(help ->
+                MapUtils.findAndThen(userMap, help.getUserId(),
+                        user -> help.setNickname(user.getNickname()).setAvatar(user.getAvatar())));
+        return helpVOs;
+    }
+    List<AppBargainHelpRespVO> convertList02(List<BargainHelpDO> helps);
+
+}
