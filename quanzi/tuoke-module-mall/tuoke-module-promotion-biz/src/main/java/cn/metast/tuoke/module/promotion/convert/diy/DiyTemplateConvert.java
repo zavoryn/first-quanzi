d2fcdc31dbcb1eca9175f951c5ce30b54b658ff9
Ming Chen <chenming202503@163.com>
2026-01-27 19:33:32 +0800

    perf: optimize database connection pool

---
+++ quanzi/tuoke-module-mall/tuoke-module-promotion-biz/src/main/java/cn/metast/tuoke/module/promotion/convert/diy/DiyTemplateConvert.java
@@ -0,0 +1,39 @@
+package cn.metast.tuoke.module.promotion.convert.diy;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.module.promotion.controller.admin.diy.vo.template.*;
+import cn.metast.tuoke.module.promotion.controller.app.diy.vo.AppDiyTemplatePropertyRespVO;
+import cn.metast.tuoke.module.promotion.dal.dataobject.diy.DiyPageDO;
+import cn.metast.tuoke.module.promotion.dal.dataobject.diy.DiyTemplateDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+/**
+ * 装修模板 Convert
+ *
+ * @author owen
+ */
+@Mapper
+public interface DiyTemplateConvert {
+
+    DiyTemplateConvert INSTANCE = Mappers.getMapper(DiyTemplateConvert.class);
+
+    DiyTemplateDO convert(DiyTemplateCreateReqVO bean);
+
+    DiyTemplateDO convert(DiyTemplateUpdateReqVO bean);
+
+    DiyTemplateRespVO convert(DiyTemplateDO bean);
+
+    List<DiyTemplateRespVO> convertList(List<DiyTemplateDO> list);
+
+    PageResult<DiyTemplateRespVO> convertPage(PageResult<DiyTemplateDO> page);
+
+    DiyTemplatePropertyRespVO convertPropertyVo(DiyTemplateDO diyTemplate, List<DiyPageDO> pages);
+
+    AppDiyTemplatePropertyRespVO convertPropertyVo2(DiyTemplateDO diyTemplate, String home, String user);
+
+    DiyTemplateDO convert(DiyTemplatePropertyUpdateRequestVO updateReqVO);
+
+}
