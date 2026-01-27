ba7dae2504aff38d889298df01ff3385508e8844
Ming Chen <chenming202503@163.com>
2026-01-27 14:56:23 +0800

    feat: add social graph follow and fan relations

---
+++ quanzi/tuoke-module-mall/tuoke-module-promotion-biz/src/main/java/cn/metast/tuoke/module/promotion/dal/mysql/banner/BannerMapper.java
@@ -0,0 +1,39 @@
+package cn.metast.tuoke.module.promotion.dal.mysql.banner;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.promotion.controller.admin.banner.vo.BannerPageReqVO;
+import cn.metast.tuoke.module.promotion.dal.dataobject.banner.BannerDO;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * Banner Mapper
+ *
+ * @author xia
+ */
+@Mapper
+public interface BannerMapper extends BaseMapperX<BannerDO> {
+
+    default PageResult<BannerDO> selectPage(BannerPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<BannerDO>()
+                .likeIfPresent(BannerDO::getTitle, reqVO.getTitle())
+                .eqIfPresent(BannerDO::getStatus, reqVO.getStatus())
+                .betweenIfPresent(BannerDO::getCreateTime, reqVO.getCreateTime())
+                .orderByDesc(BannerDO::getSort));
+    }
+
+    default void updateBrowseCount(Long id) {
+        update(null, new LambdaUpdateWrapper<BannerDO>()
+                .eq(BannerDO::getId, id)
+                .setSql("browse_count = browse_count + 1"));
+    }
+
+    default List<BannerDO> selectBannerListByPosition(Integer position) {
+        return selectList(new LambdaQueryWrapperX<BannerDO>().eq(BannerDO::getPosition, position));
+    }
+
+}
