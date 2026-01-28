0c427de60d01a6a7e9ce84ee0c40480d2e12afa6
Ming Chen <chenming202503@163.com>
2026-01-28 15:13:45 +0800

    style: unify API response format

---
+++ quanzi/tuoke-module-mall/tuoke-module-statistics-biz/src/main/java/cn/metast/tuoke/module/statistics/dal/mysql/trade/TradeStatisticsMapper.java
@@ -0,0 +1,41 @@
+package cn.metast.tuoke.module.statistics.dal.mysql.trade;
+
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.statistics.controller.admin.trade.vo.TradeTrendSummaryRespVO;
+import cn.metast.tuoke.module.statistics.dal.dataobject.trade.TradeStatisticsDO;
+import cn.metast.tuoke.module.statistics.service.trade.bo.TradeSummaryRespBO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 交易统计 Mapper
+ *
+ * @author owen
+ */
+@Mapper
+public interface TradeStatisticsMapper extends BaseMapperX<TradeStatisticsDO> {
+
+    TradeSummaryRespBO selectOrderCreateCountSumAndOrderPayPriceSumByTimeBetween(@Param("beginTime") LocalDateTime beginTime,
+                                                                                 @Param("endTime") LocalDateTime endTime);
+
+    TradeTrendSummaryRespVO selectVoByTimeBetween(@Param("beginTime") LocalDateTime beginTime,
+                                                  @Param("endTime") LocalDateTime endTime);
+
+    default List<TradeStatisticsDO> selectListByTimeBetween(LocalDateTime beginTime, LocalDateTime endTime) {
+        return selectList(new LambdaQueryWrapperX<TradeStatisticsDO>()
+                .between(TradeStatisticsDO::getTime, beginTime, endTime));
+    }
+
+    Integer selectExpensePriceByTimeBetween(@Param("beginTime") LocalDateTime beginTime,
+                                            @Param("endTime") LocalDateTime endTime);
+
+    default TradeStatisticsDO selectByTimeBetween(LocalDateTime beginTime, LocalDateTime endTime) {
+        return selectOne(new LambdaQueryWrapperX<TradeStatisticsDO>()
+                .between(TradeStatisticsDO::getTime, beginTime, endTime));
+    }
+
+}
