b1bfafb72233750e16823a46681abc0023238c6b
Ming Chen <chenming202503@163.com>
2026-01-28 19:16:10 +0800

    refactor: optimize feed query with cursor pagination

---
+++ quanzi/tuoke-module-mall/tuoke-module-statistics-biz/src/main/java/cn/metast/tuoke/module/statistics/convert/pay/PayStatisticsConvert.java
@@ -0,0 +1,19 @@
+package cn.metast.tuoke.module.statistics.convert.pay;
+
+import cn.metast.tuoke.module.statistics.controller.admin.pay.vo.PaySummaryRespVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * 支付统计 Convert
+ *
+ * @author owen
+ */
+@Mapper
+public interface PayStatisticsConvert {
+
+    PayStatisticsConvert INSTANCE = Mappers.getMapper(PayStatisticsConvert.class);
+
+    PaySummaryRespVO convert(Integer rechargePrice);
+
+}
