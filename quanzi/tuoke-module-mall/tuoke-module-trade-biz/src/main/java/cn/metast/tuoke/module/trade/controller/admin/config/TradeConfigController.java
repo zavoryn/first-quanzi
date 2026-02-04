10868cbd74d6c5f6587949f1b220e6478505caea
Ming Chen <chenming202503@163.com>
2026-02-04 08:27:05 +0800

    fix: resolve circular dependency in services

---
+++ quanzi/tuoke-module-mall/tuoke-module-trade-biz/src/main/java/cn/metast/tuoke/module/trade/controller/admin/config/TradeConfigController.java
@@ -0,0 +1,53 @@
+package cn.metast.tuoke.module.trade.controller.admin.config;
+
+import cn.metast.tuoke.framework.common.pojo.CommonResult;
+import cn.metast.tuoke.module.trade.controller.admin.config.vo.TradeConfigRespVO;
+import cn.metast.tuoke.module.trade.controller.admin.config.vo.TradeConfigSaveReqVO;
+import cn.metast.tuoke.module.trade.convert.config.TradeConfigConvert;
+import cn.metast.tuoke.module.trade.dal.dataobject.config.TradeConfigDO;
+import cn.metast.tuoke.module.trade.service.config.TradeConfigService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+
+import static cn.metast.tuoke.framework.common.pojo.CommonResult.success;
+
+@Tag(name = "管理后台 - 交易中心配置")
+@RestController
+@RequestMapping("/trade/config")
+@Validated
+public class TradeConfigController {
+
+    @Resource
+    private TradeConfigService tradeConfigService;
+
+    @Value("${tuoke.tencent-lbs-key}")
+    private String tencentLbsKey;
+
+    @PutMapping("/save")
+    @Operation(summary = "更新交易中心配置")
+    @PreAuthorize("@ss.hasPermission('trade:config:save')")
+    public CommonResult<Boolean> updateConfig(@Valid @RequestBody TradeConfigSaveReqVO updateReqVO) {
+        tradeConfigService.saveTradeConfig(updateReqVO);
+        return success(true);
+    }
+
+    @GetMapping("/get")
+    @Operation(summary = "获得交易中心配置")
+    @PreAuthorize("@ss.hasPermission('trade:config:query')")
+    public CommonResult<TradeConfigRespVO> getConfig() {
+        TradeConfigDO config = tradeConfigService.getTradeConfig();
+        TradeConfigRespVO configVO = TradeConfigConvert.INSTANCE.convert(config);
+        if (configVO != null) {
+            configVO.setTencentLbsKey(tencentLbsKey);
+        }
+        return success(configVO);
+    }
+
+}
