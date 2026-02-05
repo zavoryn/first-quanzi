817f987d0b2121be11e37e8aa0bc44b2ae417abb
Ming Chen <chenming202503@163.com>
2026-02-05 20:26:26 +0800

    feat: implement OAuth2 third-party login

---
+++ quanzi/tuoke-module-mall/tuoke-module-trade-biz/src/main/java/cn/metast/tuoke/module/trade/controller/app/brokerage/vo/record/AppBrokerageRecordPageReqVO.java
@@ -0,0 +1,31 @@
+package cn.metast.tuoke.module.trade.controller.app.brokerage.vo.record;
+
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import cn.metast.tuoke.framework.common.validation.InEnum;
+import cn.metast.tuoke.module.trade.enums.brokerage.BrokerageRecordBizTypeEnum;
+import cn.metast.tuoke.module.trade.enums.brokerage.BrokerageRecordStatusEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+import static cn.metast.tuoke.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
+
+@Schema(description = "应用 App - 分销记录分页 Request VO")
+@Data
+public class AppBrokerageRecordPageReqVO extends PageParam {
+
+    @Schema(description = "创建时间")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private LocalDateTime[] createTime;
+
+    @Schema(description = "业务类型", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
+    @InEnum(value = BrokerageRecordBizTypeEnum.class, message = "业务类型必须是 {value}")
+    private Integer bizType;
+
+    @Schema(description = "状态", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
+    @InEnum(value = BrokerageRecordStatusEnum.class, message = "状态必须是 {value}")
+    private Integer status;
+
+}
