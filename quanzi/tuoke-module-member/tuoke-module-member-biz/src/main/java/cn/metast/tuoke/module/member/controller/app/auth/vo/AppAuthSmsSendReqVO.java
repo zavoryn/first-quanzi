85ac776720e8f94032c95c021ee4d6241890cc80
Ming Chen <chenming202503@163.com>
2026-03-05 15:04:42 +0800

    test: add service layer integration tests

---
+++ quanzi/tuoke-module-member/tuoke-module-member-biz/src/main/java/cn/metast/tuoke/module/member/controller/app/auth/vo/AppAuthSmsSendReqVO.java
@@ -0,0 +1,26 @@
+package cn.metast.tuoke.module.member.controller.app.auth.vo;
+
+import cn.metast.tuoke.framework.common.validation.InEnum;
+import cn.metast.tuoke.framework.common.validation.Mobile;
+import cn.metast.tuoke.module.system.enums.sms.SmsSceneEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import jakarta.validation.constraints.NotNull;
+
+@Schema(description = "用户 APP - 发送手机验证码 Request VO")
+@Data
+@Accessors(chain = true)
+public class AppAuthSmsSendReqVO {
+
+    @Schema(description = "手机号", example = "15601691234")
+    @Mobile
+    private String mobile;
+
+    @Schema(description = "发送场景,对应 SmsSceneEnum 枚举", example = "1")
+    @NotNull(message = "发送场景不能为空")
+    @InEnum(SmsSceneEnum.class)
+    private Integer scene;
+
+}
