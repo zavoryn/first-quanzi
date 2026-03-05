8cd278f4d763aee78c3584d158e0a749c6e90ab2
Ming Chen <chenming202503@163.com>
2026-03-05 21:22:09 +0800

    style: unify API response format

---
+++ quanzi/tuoke-module-member/tuoke-module-member-biz/src/main/java/cn/metast/tuoke/module/member/convert/address/AddressConvert.java
@@ -0,0 +1,45 @@
+package cn.metast.tuoke.module.member.convert.address;
+
+import cn.metast.tuoke.framework.ip.core.utils.AreaUtils;
+import cn.metast.tuoke.module.member.api.address.dto.MemberAddressRespDTO;
+import cn.metast.tuoke.module.member.controller.admin.address.vo.AddressRespVO;
+import cn.metast.tuoke.module.member.controller.app.address.vo.AppAddressCreateReqVO;
+import cn.metast.tuoke.module.member.controller.app.address.vo.AppAddressRespVO;
+import cn.metast.tuoke.module.member.controller.app.address.vo.AppAddressUpdateReqVO;
+import cn.metast.tuoke.module.member.dal.dataobject.address.MemberAddressDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+/**
+ * 用户收件地址 Convert
+ *
+ * @author metast.cn
+ */
+@Mapper
+public interface AddressConvert {
+
+    AddressConvert INSTANCE = Mappers.getMapper(AddressConvert.class);
+
+    MemberAddressDO convert(AppAddressCreateReqVO bean);
+
+    MemberAddressDO convert(AppAddressUpdateReqVO bean);
+
+    @Mapping(source = "areaId", target = "areaName",  qualifiedByName = "convertAreaIdToAreaName")
+    AppAddressRespVO convert(MemberAddressDO bean);
+
+    List<AppAddressRespVO> convertList(List<MemberAddressDO> list);
+
+    MemberAddressRespDTO convert02(MemberAddressDO bean);
+
+    @Named("convertAreaIdToAreaName")
+    default String convertAreaIdToAreaName(Integer areaId) {
+        return AreaUtils.format(areaId);
+    }
+
+    List<AddressRespVO> convertList2(List<MemberAddressDO> list);
+
+}
