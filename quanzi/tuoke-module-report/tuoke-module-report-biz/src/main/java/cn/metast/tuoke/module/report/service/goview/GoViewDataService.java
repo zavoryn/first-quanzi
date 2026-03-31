6090a131fa61fe4417e48cf5a8ec166bc118b6a6
Ming Chen <chenming202503@163.com>
2026-04-01 07:52:20 +0800

    feat: add post publishing and draft API

---
+++ quanzi/tuoke-module-report/tuoke-module-report-biz/src/main/java/cn/metast/tuoke/module/report/service/goview/GoViewDataService.java
@@ -0,0 +1,20 @@
+package cn.metast.tuoke.module.report.service.goview;
+
+import cn.metast.tuoke.module.report.controller.admin.goview.vo.data.GoViewDataRespVO;
+
+/**
+ * GoView 数据 Service 接口
+ *
+ * @author metast.cn
+ */
+public interface GoViewDataService {
+
+    /**
+     * 使用 SQL 查询数据
+     *
+     * @param sql SQL 语句
+     * @return 数据
+     */
+    GoViewDataRespVO getDataBySQL(String sql);
+
+}
