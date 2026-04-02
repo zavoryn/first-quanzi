b38803683098351849fc9732eb3698b6c68894af
Ming Chen <chenming202503@163.com>
2026-04-02 21:52:38 +0800

    feat: add post publishing and draft API

---
+++ quanzi/tuoke-module-system/tuoke-module-system-api/src/main/java/cn/metast/tuoke/module/system/enums/oauth2/OAuth2ClientConstants.java
@@ -0,0 +1,13 @@
+package cn.metast.tuoke.module.system.enums.oauth2;
+
+/**
+ * OAuth2.0 客户端的通用枚举
+ *
+ * @author metast.cn
+ */
+public interface OAuth2ClientConstants {
+
+    String CLIENT_ID_DEFAULT = "default";
+    String CLIENT_ID_APP = "app";
+
+}
