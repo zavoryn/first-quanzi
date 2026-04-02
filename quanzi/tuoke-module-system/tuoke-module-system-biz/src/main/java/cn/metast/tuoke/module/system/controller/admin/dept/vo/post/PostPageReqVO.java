504fd7fac3f991dc2b59845e412e79e529ac1dc5
Ming Chen <chenming202503@163.com>
2026-04-02 18:59:51 +0800

    feat: add social graph follow and fan relations

---
+++ quanzi/tuoke-module-system/tuoke-module-system-biz/src/main/java/cn/metast/tuoke/module/system/controller/admin/dept/vo/post/PostPageReqVO.java
@@ -0,0 +1,22 @@
+package cn.metast.tuoke.module.system.controller.admin.dept.vo.post;
+
+import cn.metast.tuoke.framework.common.pojo.PageParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Schema(description = "管理后台 - 岗位分页 Request VO")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostPageReqVO extends PageParam {
+
+    @Schema(description = "岗位编码，模糊匹配", example = "tuoke")
+    private String code;
+
+    @Schema(description = "岗位名称，模糊匹配", example = "元圈")
+    private String name;
+
+    @Schema(description = "展示状态，参见 CommonStatusEnum 枚举类", example = "1")
+    private Integer status;
+
+}
