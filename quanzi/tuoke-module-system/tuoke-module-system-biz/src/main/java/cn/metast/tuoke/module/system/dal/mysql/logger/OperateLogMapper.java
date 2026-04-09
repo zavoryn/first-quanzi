c4ef9f0c88abff9e46ba89c6a02caddf21f36b12
Ming Chen <chenming202503@163.com>
2026-04-09 10:24:58 +0800

    refactor: optimize feed query with cursor pagination

---
+++ quanzi/tuoke-module-system/tuoke-module-system-biz/src/main/java/cn/metast/tuoke/module/system/dal/mysql/logger/OperateLogMapper.java
@@ -0,0 +1,79 @@
+package cn.metast.tuoke.module.system.dal.mysql.logger;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.system.api.logger.dto.OperateLogPageReqDTO;
+import cn.metast.tuoke.module.system.controller.admin.logger.vo.operatelog.OperateLogPageReqVO;
+import cn.metast.tuoke.module.system.dal.dataobject.logger.OperateLogDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OperateLogMapper extends BaseMapperX<OperateLogDO> {
+
+    default PageResult<OperateLogDO> selectPage(OperateLogPageReqVO pageReqDTO) {
+        return selectPage(pageReqDTO, new LambdaQueryWrapperX<OperateLogDO>()
+                .eqIfPresent(OperateLogDO::getUserId, pageReqDTO.getUserId())
+                .eqIfPresent(OperateLogDO::getBizId, pageReqDTO.getBizId())
+                .likeIfPresent(OperateLogDO::getType, pageReqDTO.getType())
+                .likeIfPresent(OperateLogDO::getSubType, pageReqDTO.getSubType())
+                .likeIfPresent(OperateLogDO::getAction, pageReqDTO.getAction())
+                .betweenIfPresent(OperateLogDO::getCreateTime, pageReqDTO.getCreateTime())
+                .orderByDesc(OperateLogDO::getId));
+    }
+
+    default PageResult<OperateLogDO> selectPageByUserIds(OperateLogPageReqVO pageReqDTO, List<Long> userIds) {
+        return selectPage(pageReqDTO, new LambdaQueryWrapperX<OperateLogDO>()
+                .inIfPresent(OperateLogDO::getUserId, userIds)
+                .eqIfPresent(OperateLogDO::getUserId, pageReqDTO.getUserId())
+                .eqIfPresent(OperateLogDO::getBizId, pageReqDTO.getBizId())
+                .likeIfPresent(OperateLogDO::getType, pageReqDTO.getType())
+                .likeIfPresent(OperateLogDO::getSubType, pageReqDTO.getSubType())
+                .likeIfPresent(OperateLogDO::getAction, pageReqDTO.getAction())
+                .betweenIfPresent(OperateLogDO::getCreateTime, pageReqDTO.getCreateTime())
+                .orderByDesc(OperateLogDO::getId));
+    }
+
+    default PageResult<OperateLogDO> selectPageByOrderIds(OperateLogPageReqVO pageReqDTO, List<Long> orderIds) {
+        return selectPage(pageReqDTO, new LambdaQueryWrapperX<OperateLogDO>()
+                .inIfPresent(OperateLogDO::getUserId, orderIds)
+                .eqIfPresent(OperateLogDO::getUserId, pageReqDTO.getUserId())
+                .eqIfPresent(OperateLogDO::getBizId, pageReqDTO.getBizId())
+                .likeIfPresent(OperateLogDO::getType, pageReqDTO.getType())
+                .likeIfPresent(OperateLogDO::getSubType, pageReqDTO.getSubType())
+                .likeIfPresent(OperateLogDO::getAction, pageReqDTO.getAction())
+                .betweenIfPresent(OperateLogDO::getCreateTime, pageReqDTO.getCreateTime())
+                .orderByDesc(OperateLogDO::getId));
+    }
+
+    default PageResult<OperateLogDO> selectPage(OperateLogPageReqDTO pageReqDTO) {
+        return selectPage(pageReqDTO, new LambdaQueryWrapperX<OperateLogDO>()
+                .eqIfPresent(OperateLogDO::getType, pageReqDTO.getType())
+                .eqIfPresent(OperateLogDO::getBizId, pageReqDTO.getBizId())
+                .eqIfPresent(OperateLogDO::getUserId, pageReqDTO.getUserId())
+                .orderByDesc(OperateLogDO::getId));
+    }
+
+    default PageResult<OperateLogDO> selectPageByOrderIds(OperateLogPageReqDTO pageReqDTO, List<Long> orderIds) {
+        return selectPage(pageReqDTO, new LambdaQueryWrapperX<OperateLogDO>()
+                .inIfPresent(OperateLogDO::getUserId, orderIds)
+                .eqIfPresent(OperateLogDO::getType, pageReqDTO.getType())
+                .eqIfPresent(OperateLogDO::getBizId, pageReqDTO.getBizId())
+                .eqIfPresent(OperateLogDO::getUserId, pageReqDTO.getUserId())
+                .orderByDesc(OperateLogDO::getId));
+    }
+
+    default PageResult<OperateLogDO> selectPageByUserIds(OperateLogPageReqDTO pageReqDTO, List<Long> userIds) {
+        return selectPage(pageReqDTO, new LambdaQueryWrapperX<OperateLogDO>()
+                .inIfPresent(OperateLogDO::getUserId, userIds)
+                .eqIfPresent(OperateLogDO::getType, pageReqDTO.getType())
+                .eqIfPresent(OperateLogDO::getBizId, pageReqDTO.getBizId())
+                .eqIfPresent(OperateLogDO::getUserId, pageReqDTO.getUserId())
+                .orderByDesc(OperateLogDO::getId));
+    }
+
+}
+
+
---
+++ quanzi/tuoke-module-system/tuoke-module-system-biz/src/main/java/cn/metast/tuoke/module/system/dal/mysql/mail/MailAccountMapper.java
@@ -0,0 +1,20 @@
+package cn.metast.tuoke.module.system.dal.mysql.mail;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.QueryWrapperX;
+import cn.metast.tuoke.module.system.controller.admin.mail.vo.account.MailAccountPageReqVO;
+import cn.metast.tuoke.module.system.dal.dataobject.mail.MailAccountDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MailAccountMapper extends BaseMapperX<MailAccountDO> {
+
+    default PageResult<MailAccountDO> selectPage(MailAccountPageReqVO pageReqVO) {
+        return selectPage(pageReqVO, new LambdaQueryWrapperX<MailAccountDO>()
+                .likeIfPresent(MailAccountDO::getMail, pageReqVO.getMail())
+                .likeIfPresent(MailAccountDO::getUsername , pageReqVO.getUsername()));
+    }
+
+}
