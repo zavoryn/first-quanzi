3c32f6a8c2e7d1e44209fd32fe6252658d2199a2
Ming Chen <chenming202503@163.com>
2026-04-09 21:55:13 +0800

    refactor: optimize feed query with cursor pagination

---
+++ quanzi/tuoke-module-system/tuoke-module-system-biz/src/main/java/cn/metast/tuoke/module/system/dal/mysql/sms/SmsLogMapper.java
@@ -0,0 +1,25 @@
+package cn.metast.tuoke.module.system.dal.mysql.sms;
+
+import cn.metast.tuoke.framework.common.pojo.PageResult;
+import cn.metast.tuoke.framework.mybatis.core.mapper.BaseMapperX;
+import cn.metast.tuoke.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.metast.tuoke.module.system.controller.admin.sms.vo.log.SmsLogPageReqVO;
+import cn.metast.tuoke.module.system.dal.dataobject.sms.SmsLogDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SmsLogMapper extends BaseMapperX<SmsLogDO> {
+
+    default PageResult<SmsLogDO> selectPage(SmsLogPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<SmsLogDO>()
+                .eqIfPresent(SmsLogDO::getChannelId, reqVO.getChannelId())
+                .eqIfPresent(SmsLogDO::getTemplateId, reqVO.getTemplateId())
+                .likeIfPresent(SmsLogDO::getMobile, reqVO.getMobile())
+                .eqIfPresent(SmsLogDO::getSendStatus, reqVO.getSendStatus())
+                .betweenIfPresent(SmsLogDO::getSendTime, reqVO.getSendTime())
+                .eqIfPresent(SmsLogDO::getReceiveStatus, reqVO.getReceiveStatus())
+                .betweenIfPresent(SmsLogDO::getReceiveTime, reqVO.getReceiveTime())
+                .orderByDesc(SmsLogDO::getId));
+    }
+
+}
