12927ec10859728882cf88ac041339a546cfecb4
Ming Chen <chenming202503@163.com>
2026-04-30 07:07:52 +0800

    refactor: extract base service and DAO layers

---
+++ quanzi-ui/src/App.vue
@@ -0,0 +1,71 @@
+<script lang="ts" setup>
+import { isDark } from '@/utils/is'
+import { useAppStore } from '@/store/modules/app'
+import { useDesign } from '@/hooks/web/useDesign'
+import { CACHE_KEY, useCache } from '@/hooks/web/useCache'
+import routerSearch from '@/components/RouterSearch/index.vue'
+import { setCssVar } from '@/utils'
+
+defineOptions({ name: 'APP' })
+
+const { getPrefixCls } = useDesign()
+const prefixCls = getPrefixCls('app')
+const appStore = useAppStore()
+const currentSize = computed(() => appStore.getCurrentSize)
+const greyMode = computed(() => appStore.getGreyMode)
+const { wsCache } = useCache()
+
+// 根据浏览器当前主题设置系统主题色
+const setDefaultTheme = () => {
+  let isDarkTheme = wsCache.get(CACHE_KEY.IS_DARK)
+  if (isDarkTheme === null) {
+    isDarkTheme = isDark()
+  }
+  appStore.setIsDark(isDarkTheme)
+}
+setDefaultTheme()
+// console.log('==>initinti', wsCache.get(CACHE_KEY.THEME))
+// 头部主题默认黑色
+if (!wsCache.get(CACHE_KEY.THEME)) {
+  setCssVar('--top-header-bg-color', '#151515')
+  setCssVar('--top-header-text-color', '#fff')
+  setCssVar('--top-header-hover-color', '#151515')
+  appStore.setTheme({  
+    topHeaderBgColor: '#151515',
+    topHeaderTextColor: '#fff',
+    topHeaderHoverColor: '#151515',
+  })
+}
+
+</script>
+<template>
+  <ConfigGlobal :size="currentSize">
+    <RouterView :class="greyMode ? `${prefixCls}-grey-mode` : ''" />
+    <routerSearch />
+  </ConfigGlobal>
+</template>
+<style lang="scss">
+$prefix-cls: #{$namespace}-app;
+
+.size {
+  width: 100%;
+  height: 100%;
+}
+
+html,
+body {
+  @extend .size;
+
+  padding: 0 !important;
+  margin: 0;
+  overflow: hidden;
+
+  #app {
+    @extend .size;
+  }
+}
+
+.#{$prefix-cls}-grey-mode {
+  filter: grayscale(100%);
+}
+</style>
